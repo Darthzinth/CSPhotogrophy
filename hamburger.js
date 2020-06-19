6a0cc3ac075ee4b124794f8d8abc8cd46e00ac79
Caleb Stoeltzing <65977944+Darthzinth@users.noreply.github.com>
2020-06-19 10:37:34 -0400

    Add files via upload

---
+++ hamburger.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+    $('.js--fav-images').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky')
+        }   
+    }, {
+        offset: '40px'
+    });
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--secondary-nav');
+        var icon = $('.js--nav-icon');
+        
+        nav.slideToggle(200);
+        
+        if (icon.attr('name') == "menu") {
+            icon.attr('name', 'close-sharp');
+        } else {
+            icon.attr('name', 'menu')
+        }
+    });
+});
